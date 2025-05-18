806d420e06571f6241922495cc19776788014cfe
Esteban R <dragoartworks@gmailcom>
2025-05-18 17:22:12 -0500

    h

--- farmacia/settings.py
+++ farmacia/settings.py
@@ -17,7 +17,7 @@ ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME','127.0.0.1', 'localho
 
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
